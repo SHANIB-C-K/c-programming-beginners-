13f7d6c478a95c76b364cb20f9d089adb54108ec
SHANIB C K <134039228+SHANIBCK@users.noreply.github.com>
2023-09-17 12:37:26 +0530

    Update print-helloworld.c

--- print-helloworld.c
+++ print-helloworld.c
@@ -1,4 +1,16 @@
+//This is a simple program 
+//Program to print Hello world
+
 #include<stdio.h>
 void main () {
   printf("Hello World");
 }
+
+
+/*Output 👇👇👇
+
+-----------------------
+Hello World
+-----------------------
+
+*/
