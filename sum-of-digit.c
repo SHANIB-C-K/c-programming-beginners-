6f50c5b93b82191f3d7309af9cf7a563198a5f70
SHANIB C K <134039228+SHANIBCK@users.noreply.github.com>
2023-09-17 15:08:01 +0530

    Create sum-of-digit.c

---
+++ sum-of-digit.c
@@ -0,0 +1,15 @@
+//This is the simplest program in c 
+//Program to find sum of digit of a number 
+
+#include<stdio.h>
+void main () {
+  int num, rem, sum = 0;
+  printf("Enter the numbers : ");
+  scanf("%d", &num);
+  while(num != 0) {
+    rem = num % 10;
+    sum = sum + rem;
+    num = num / 10;
+  }
+  printf("Sum of digit = %d", sum);
+}
