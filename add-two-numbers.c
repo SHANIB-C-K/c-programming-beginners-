59a8b74dac5876696782d8f87368dd3057718dac
SHANIB C K <134039228+SHANIBCK@users.noreply.github.com>
2023-09-17 12:44:26 +0530

    Create add-two-numbers.c

---
+++ add-two-numbers.c
@@ -0,0 +1,21 @@
+//This is the simplest program in c 
+//program to find sum in two numbers
+
+#include<stdio.h>
+void main () {
+  int num1, num2, sum;
+  printf("Enter two numbers : ");
+  scanf("%d%d", &num1, &num2);
+  sum = num1 + num2;
+  printf("Result = %d", sum);
+}
+
+/* Output
+
+-----------------------------
+  Enter two numbers : 10
+  5
+  Result = 15
+-----------------------------
+
+  */
